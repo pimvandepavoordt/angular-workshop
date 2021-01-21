03c6687b237e3688764f0b238e7c768e28f72c2c
pimvandepavoordt <pimvandepavoordt@gmail.com>
2021-01-21 10:52:56 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from "./app.component";
 import { InterestRatesComponent } from "./interest-rates/interest-rates.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HelloComponent } from "./hello.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, InterestRatesComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    InterestRatesComponent,
+    DashboardComponent,
+    HelloComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
