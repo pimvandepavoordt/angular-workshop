e119c90767db529ed3fa22f2b2ae1bc1b875dd0c
pimvandepavoordt <pimvandepavoordt@gmail.com>
2021-01-21 11:45:36 +0100

    2 files modified

--- src/app/interest-rates/interest-rates.component.ts
+++ src/app/interest-rates/interest-rates.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-interest-rates',
-  templateUrl: './interest-rates.component.html',
-  styleUrls: ['./interest-rates.component.css']
+  selector: "app-interest-rates",
+  templateUrl: "./interest-rates.component.html",
+  styleUrls: ["./interest-rates.component.css"]
 })
 export class InterestRatesComponent implements OnInit {
+  interestRatesList = [
+    { rateClass: "NHG", rate: "1" },
+    { rateClass: "t/m 67,50% MW", rate: "1.3" },
+    { rateClass: "t/m 81% MW", rate: "1.5" },
+    { rateClass: "meer dan 81%", rate: "1.8" }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
